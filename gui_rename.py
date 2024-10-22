edb0c3467c31253183b4111661b3aed2f2c329db
GaneshKategar <153308027+GaneshKategar@users.noreply.github.com>
2024-10-23 00:31:09 +0530

    gui_rename.py

--- gui_rename.py
+++ gui_rename.py
@@ -1,13 +1,17 @@
 from tkinter import *
-from tkinter import ttk,filedialog
+from tkinter import filedialog
 import tkinter as tk
 import time
 import webbrowser
 import os
+import threading
 import requests
+
 
 def clicked():
     global stnum,ltnum,idx
+    global file_list
+    file_list = []
     idx = x.get()
     stnum= st.get()
     ltnum = lt.get()
@@ -32,11 +36,13 @@ def ui():
     x = tk.StringVar() 
     st = tk.StringVar()
     lt = tk.StringVar()
+    img = PhotoImage(file=r"D:\Ganesh\Something\New_folder\Screenshot28.png")
     #Declaring all the widgets
     title = tk.Label(root,text="WELCOME TO ANIME EPISODE RENAMER")
     desc = tk.Label(root,text="This is used to rename episodes of an anime to thier actual numbers rather than episode numbers")
-    help = tk.Label(root,text="If you need any help regarding how to use this app click this text to know how to use it")
-    help.bind("<Button-1>",lambda e: webbrowser.open_new("https://github.com/GaneshKategar/anime-renamer/blob/main/README.md"))
+    helpimg = tk.Label(root,text="If you need any help regarding how to use this app click this text to know how to use it",image=img,cursor='dot',width=10,height=20)
+    # helpimg = tk.Label(root,image=img)
+    helpimg.bind("<Button-1>",lambda e: webbrowser.open_new("https://github.com/GaneshKategar/anime-renamer/blob/main/README.md"))
 
     warn = tk.Label(root,text="These are the to enter to make the app work correctly")
 
@@ -55,7 +61,7 @@ def ui():
     bro_label = tk.Label(root,text="Browse to the directoy")
     bro_btn = tk.Button(root,text="browse",command=selectdir)
 
-    sbt_btn = tk.Button(root,text="Submit",command=clicked)
+    sbt_btn = tk.Button(root,text="Submit",command=threading.Thread(target=clicked).start)
 
     res = tk.Label(root,textvariable='')
 
@@ -64,7 +70,8 @@ def ui():
     #Positioning the widgets
     title.pack(ipadx=20,ipady=20,anchor=tk.S)
     desc.pack()
-    help.pack()
+    helpimg.pack()
+    
     warn.pack(ipady=20)
     
     id_label.pack()
@@ -88,64 +95,86 @@ def ui():
 #Renaming the files
 def rename():
     data = data_fecthing()
+    print(file_list)
     srcdir = os.listdir(dir)
     res.configure(text="The files are being renamed")
+    #Checking if there are sufficient files to rename
     if len(srcdir) < (ltnum-stnum):
         print("The directory does not have sufficient files")
         return
-    with open("te.txt","r") as file:
-        
-        ds = file.readline()
-        for l in range(0,(data[1]-data[0]+1)):  
-            src = os.path.join(dir,srcdir[l])
-            j = ds.split("__")
-            dst = dir+"/" + j[l] + ".mkv"
-            try:
-                if os.path.exists(src):
-                    os.rename(src,dst)
-                    print(src)
-                    print(dst)
-                    print("File Renamed",l,"\n")
-            except Exception as e:
-                print(e)
-    res.configure("The files have successfully renamed")
-
+    #Renaming the files
+    for l in range(0,(data[1]-data[0]+1)):  
+        ds = file_list[l]
+        src = os.path.join(dir,srcdir[l])
+        dst = dir+ "\\" + ds + ".mkv"
+        print(dst)
+        try:
+            if os.path.exists(src):
+                os.rename(src,dst)
+                print(src)
+                print(dst)
+                print("File Renamed",l,"\n")
+        except Exception as e:
+            print("HLELO MORO",e)
+    res.configure(text="The files have successfully renamed")
+    print("The files have successfully renamed")
 
 
 #Get data from the web
-def openweb(url):
+def openweb(url,tit_url):
 
     response = requests.get(url)
+    title_res = requests.get(tit_url)
 
-    if response.status_code != 200:
+    #to get status if we connected successfully with the api
+    if response.status_code != 200 or title_res.status_code !=200:
         print("cannot access web")
         print(response)
         return 
     else:
+        #to get data about the anime i.e the name of anime for example in 269 we have anime title "bleach"
+        title_data = title_res.json()
+        title_data = title_data['data']
+        title_data = title_data['titles']
+        title_data = title_data[0]
+
+        #to get data about the episode that we want to rename
         data = response.json()
         data = data['data']
-        epi = {'mal_id':data['mal_id'],'title':data['title']}
-        return epi['mal_id'],epi['title']
+        epi = {'Ani_title':title_data['title'] ,'mal_id':data['mal_id'] ,'title':data['title'],'filler':data['filler'] }   # Anime title = Name of anine ,mal_id = episode number, title = episode title/name
+        return epi['Ani_title'],epi['mal_id'],epi['title'],epi['filler']
 
 
 # To Enter the episode names in a file
 def data_fecthing():
+    global file_list
     li = [stnum,ltnum]
     print("Please Wait while we fetch the data.................")
     res.configure(text="The data is being fetched.Please Wait......")
-    file = open('te.txt','w') 
+
     for i in range(stnum,ltnum+1):
+        temp_list = ''
         url = f'https://api.jikan.moe/v4/anime/{idx}/episodes/{i}'
+        tit_url = f'https://api.jikan.moe/v4/anime/{idx}'
         try:
-            openweb(url)
-            ep,tit = openweb(url)
-            file.write(str(ep))
-            file.write("-")
-            while '?' in tit:
+            #sends 2 url's to get data about the episode and the name of anime and recives 4 inputs
+            ani,ep,tit,fill = openweb(url,tit_url)
+            while ':' in ani:                       #os.rename gives error when we rename file name with a text that has ":" in it
+                ani = ani.replace(':'," ")
+            temp_list+=ani
+            temp_list+=" "
+            if fill:
+                temp_list+='[Filler]'
+                temp_list+=' '
+            temp_list+=str(ep)
+            temp_list+="-"
+            while '?' in tit:                     #os.rename gives error when we rename file name with a text that has "?" in it
                 tit = tit.replace("?","!")
-            file.write(tit)
-            file.write("__")
+            temp_list+=tit
+            # temp_list.append("__")
             time.sleep(0.8)
+            print(temp_list)
+            file_list.append(temp_list)
             print("entered data in the file ")
         except Exception as e:
             print(e)
